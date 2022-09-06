a90d9e41849331afe71fc98247a04f5470f7ab90
Aswin Haridas <aswinvharidas@gmail.com>
2022-09-06 18:30:32 +0530

    Adding all files to commit

---
+++ Trainticketgenerate/DBConnection.java
@@ -0,0 +1,22 @@
+package Trainticketgenerate;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static String url= "jdbc:mysql://localhost:3306/trainticketpnr?useSSL=false";
+    private static String user ="root";
+    private static String password = "root";
+    private static Connection connection = null;
+
+    public  Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
